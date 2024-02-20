94a33ac87b4e5fd7aed1da248f5e8fca64856a90
chanhihi <chanhihi55@gmail.com>
2024-02-20 15:34:55 +0900

    mp00

---
+++ ex02/ex02/ex02App.swift
@@ -0,0 +1,17 @@
+//
+//  ex02App.swift
+//  ex02
+//
+//  Created by Chan on 2/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ex02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
